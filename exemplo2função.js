6d8bee946f379caeff2974d99a795cdae006f7c7
Hernanes Junior <108368859+HernanesJunior@users.noreply.github.com>
2024-01-24 19:58:59 -0400

    exemplo2função.js

---
+++ exemplo2função.js
@@ -0,0 +1,18 @@
+function calcularIMC(peso,altura){
+}
+
+function classificarIMC(imc){
+    
+}
+
+function main(){
+const peso = 80;
+const altura = 1.74;
+
+const imc = calcularIMC(peso,altura);  /* puxa  a função que foi criada em cima */
+console.log(classificarIMC(imc));
+}
+main = function(){
+    console.log(1);
+}
+ main();
